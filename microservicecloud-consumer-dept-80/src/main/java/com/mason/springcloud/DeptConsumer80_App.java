eeff58ecd080a14bbb9ddd89f0e06c8fa6a022c7
guofei.wu <guofei.wu@ucarinc.com>
2019-01-30 12:01:15 +0800

    Ribbon负载均衡理解

--- microservicecloud-consumer-dept-80/src/main/java/com/mason/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/mason/springcloud/DeptConsumer80_App.java
@@ -10,8 +10,8 @@ import org.springframework.cloud.netflix.ribbon.RibbonClients;
 @SpringBootApplication
 @EnableEurekaClient
 //@RibbonClient(name = "MICROSERVICE-DEPT", configuration = MyRules.class)
-//@RibbonClient(name = "microservice-dept", configuration = MyRules.class) // 名称小写无效
-@RibbonClients(value = {@RibbonClient(name = "MICROSERVICE-DEPT", configuration = MyRules.class)})
+@RibbonClient(name = "microservice-dept", configuration = MyRules.class) // 若是小写名称，Controller中的url中的也要是小写。
+//@RibbonClients(value = {@RibbonClient(name = "MICROSERVICE-DEPT", configuration = MyRules.class)})
 public class DeptConsumer80_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer80_App.class, args);
--- microservicecloud-consumer-dept-80/src/main/java/com/mason/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/mason/springcloud/controller/DeptController_Consumer.java
@@ -14,7 +14,8 @@ import org.springframework.web.client.RestTemplate;
 public class DeptController_Consumer {
 
     //	private static final String REST_URL_PREFIX = "http://localhost:8001";
-    private static final String REST_URL_PREFIX = "http://MICROSERVICE-DEPT";
+//    private static final String REST_URL_PREFIX = "http://MICROSERVICE-DEPT";
+    private static final String REST_URL_PREFIX = "http://microservice-dept";
     /**
      * 使用 使用restTemplate访问restful接口非常的简单粗暴无脑。 (url, requestMap,
      * ResponseBean.class)这三个参数分别代表 REST请求地址、请求参数、HTTP响应转换被转换成的对象类型。
